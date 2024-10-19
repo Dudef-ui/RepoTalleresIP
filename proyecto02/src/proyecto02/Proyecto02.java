c2e452aecd3e127977303b8187e4ab5b1e4c6d79
Dagonzález2006 <dudef.2222.11@gmail.com>
2024-10-18 21:22:29 -0500

    tercer comentario, creacion del segundo proyecto en neatbeans trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,8 @@
+package proyecto02;
+public class Proyecto02 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos basico de java versionado a traves de Git, manejado desde GitHub ademas se usar el GitHubClassroom ");
+        
+    }
+    
+}
